fd16b78186c263c14fdb03182738b1526591c8a2
Murad Ali <murad.manni@gmail.com>
2022-03-21 13:04:52 +1100

    All features finally added and fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,73 @@
 var displayDateTime = $("#currentDay");
 var divTimeBlockContainer =$("#container");
 //var workSchedule= $("#schedule");
-var workHour=8;
-var workDescription="adsdsa";
+var workHour="";
+var workDescription="";
 
 // moment().format("h:mm");
 var startTime=8;
 var workingHourInDay=10;
 
-var workSchedule = {
-    hour: "",
-    description:""
-};
+var timeDisplayInRow;
+var currentHour= moment().format("H");
+var colorRow;
+
+var workScheduleArray = new Array();
 
 displayDateTime.text(moment().format("dddd MMMM Do, YYYY"));
 
-
-$("#displayTime").text(startTime);
-
-if (startTime>="9:45")
-{
-    console.log("true");
-}
-
 function generateSchedule(){
+    //alert(moment().format("hh:mm a"));
     var timeBlockTable= $("<table>")
         .attr("id","schedule")
         timeBlockTable.addClass("col-12");
 
-    var storageWorkSchedule = JSON.parse(localStorage.getItem("workSch"));
+    var storageWorkSchedule = JSON.parse(localStorage.getItem("localStorageWorkSchedule"));
     if (storageWorkSchedule !==null)
     {
-        console.log("enter hogaya");
-        workSchedule=storageWorkSchedule;
+        workScheduleArray=storageWorkSchedule;
     }
 
     for(var i=0; i<workingHourInDay; i++)
     {
+        timeDisplayInRow=(startTime+i) + ":00";
+        workHour = moment(timeDisplayInRow,"hh:mm").format("hh:mm a");  //Changing the format using moment to display time.
+        currentHour= parseInt(moment().format("H"));  //Getting value of current hour in order to compare and set color for row (past, present or future)
+        
+        // Condition to check if the CurrentHour is present, past or future and set the class accordingly
+        //used a variable to change the value depending on the hour
+        
+        if (currentHour === (startTime+i))
+        {
+            colorRow="present";
+        }
+        else if(currentHour > (startTime+i))
+        {
+            colorRow="past";
+        }
+        else{
+            colorRow="future";
+        }
+
+        
         var newRow = $('<tr>')
-            .addClass("row");
+            .addClass("row ");
     
-        var time=(startTime+i) + ":00";
-        workHour = moment(time,"hh:mm").format("hh:mm a");
         var newRowTime = $('<td>')
-            .addClass("hour time-block col-2")
+            .addClass("hour time-block col-2 ")
             .attr("id","displayTime")
             .text(workHour);
-            
-
-        // var desc = workSchedule.find(desc => desc.hour === workHour);
-
-        var newRowDetail = $('<td>')
-            .addClass("description past col-8");
-            
+        
+        var newRowDescription = $('<td>')
+            .addClass("description past col-8 "  + colorRow);
+        
+        //Finding the time in the workScheduleArray and getting the value or description related to that time and then display.
+        workDescription = workScheduleArray.find((desc) => desc.hour === workHour)
         var description = $("<textarea>")
-            // .text(desc.description)
+            .text(workDescription.description)
             .addClass("work");
 
-                newRowDetail.append(description);
+            newRowDescription.append(description);
 
         var newRowDetailButton = $('<td>')
             .addClass("col-2 saveBtn saveBtn1");
@@ -71,7 +81,7 @@ function generateSchedule(){
             button.append(buttonImage);
             newRowDetailButton.append(button);
 
-        newRow.append(newRowTime,newRowDetail,newRowDetailButton);
+        newRow.append(newRowTime,newRowDescription,newRowDetailButton);
         timeBlockTable.append(newRow);
         divTimeBlockContainer.append(timeBlockTable);
         
@@ -83,26 +93,42 @@ divTimeBlockContainer.on('click','.saveBtn1',alter);
 
 function alter(event){
     var t = $(event.target);
-    var thirdRow=t.parent(0);
-
-    console.log(thirdRow);
-      // Save related form data as an object
     var currentRow=$(this).closest("tr"); 
-         
-    var col1=currentRow.find("td:eq(0)").text();
-    var col2=currentRow.find("td:eq(1)").html();
-    //var data = $(this).parent().prev("td").find("textarea").val();     
-    var data =$(this).closest('tr').find('textarea').val();
-    alert(data); 
+    var time=currentRow.find("td:eq(0)").text();   
+    var description =$(this).closest('tr').find('textarea').val();
 
-    console.log(col1 + "    " + col2 +   "     ");
-
-    var workSchedule = {
-        hour: "a",
-        description:"a"
-    };
-    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-    localStorage.setItem("workSch", JSON.stringify(workSchedule));
-  
+    saveToLocalStorage(time, description);  //Passing time and description value to the function
 }
-generateSchedule();
+
+function saveToLocalStorage(workHour, workDescription){
+
+    workScheduleArray = JSON.parse(localStorage.getItem("localStorageWorkSchedule"));
+    
+    var index = workScheduleArray.findIndex(desc => desc.hour === workHour);
+    workScheduleArray[index].description=workDescription;
+    
+    localStorage.setItem("localStorageWorkSchedule", JSON.stringify(workScheduleArray));
+    
+}
+
+function saveEmptyToLocalStorage(){
+    var storageWorkSchedule = JSON.parse(localStorage.getItem("localStorageWorkSchedule"));
+    
+    if (storageWorkSchedule ===null)
+    {
+        for(var i=0; i<workingHourInDay; i++)
+        {
+            var time=(startTime+i) + ":00";
+            workHour = moment(time,"hh:mm").format("hh:mm a");
+            workScheduleArray.push({hour:workHour, description:""});
+        }
+        localStorage.setItem("localStorageWorkSchedule", JSON.stringify(workScheduleArray));   
+    }
+}
+
+function init(){
+    saveEmptyToLocalStorage();
+    generateSchedule();
+}
+
+init();
